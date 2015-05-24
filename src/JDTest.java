6127d16b88835bb6826b6be8ea1c10a424754019
Ambrose <a.cole@mail.cryst.bbk.ac.uk>
2015-05-24 12:48:38 +0100

    Initial commit

---
+++ src/JDTest.java
@@ -0,0 +1,14 @@
+/**
+ * Created by digibrose on 24/05/2015.
+ */
+public interface JDTest {
+
+
+    /**
+     * Some Method to test JavaDoc
+     *
+     */
+
+    int returnAnumber(int i);
+
+}
---
+++ src/JDTestImpl.java
@@ -0,0 +1,16 @@
+/**
+ * Created by digibrose on 24/05/2015.
+ */
+public class JDTestImpl implements JDTest {
+
+    /**
+     * Here's the class javadoc
+     * @param i
+     * @return
+     */
+
+    @Override
+    public int returnAnumber(int i) {
+        return 0;
+    }
+}
